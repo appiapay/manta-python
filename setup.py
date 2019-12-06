b649132e654d7468938907c7df160c579361372f
Alessandro Viganò <alvistar@gmail.com>
2019-12-06 08:25:49 +0100

    Added git hook Changed version

--- setup.py
+++ setup.py
@@ -12,31 +12,29 @@ from manta import MANTA_VERSION
 
 here_dir = os.path.dirname(__file__)
 
-with open(os.path.join(here_dir, 'requirements.txt')) as req_file:
+with open(os.path.join(here_dir, "requirements.txt")) as req_file:
     requirements = req_file.read().splitlines()
 
-with open(os.path.join(here_dir, 'requirements-tests.txt')) as req_file:
+with open(os.path.join(here_dir, "requirements-tests.txt")) as req_file:
     requirements_tests = req_file.read().splitlines()
 
-with open(os.path.join(here_dir, 'README.rst'), encoding='utf-8') as f:
+with open(os.path.join(here_dir, "README.rst"), encoding="utf-8") as f:
     README = f.read()
 
 
 setup(
-    name='manta',
-    version='1.6.1',
+    name="manta",
+    version="1.6.2",
     description="Manta protocol components",
     long_description=README,
-    packages=['manta', 'manta.testing'],
-    url='https://appiapay.github.io/manta-python',
-    license='BSD',
-    author='Alessandro Viganò',
-    author_email='alvistar@gmail.com',
+    packages=["manta", "manta.testing"],
+    url="https://appiapay.github.io/manta-python",
+    license="BSD",
+    author="Alessandro Viganò",
+    author_email="alessandro@appia.co",
     install_requires=requirements,
-    extras_require={
-        'runner':  requirements_tests,
-    },
-    python_requires='>=3.7',
+    extras_require={"runner": requirements_tests,},
+    python_requires=">=3.7",
     classifiers=[
         "Development Status :: 4 - Beta",
         "Programming Language :: Python :: 3",
@@ -44,15 +42,14 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: BSD License",
         "Topic :: System :: Networking",
-        "Topic :: Office/Business :: Financial :: Point-Of-Sale"
-        ],
+        "Topic :: Office/Business :: Financial :: Point-Of-Sale",
+    ],
     entry_points={
-        'console_scripts': [
-            'manta-runner=manta.testing.__main__:main',
-            'manta-store=manta.testing.__main__:store_main',
-            'manta-payproc=manta.testing.__main__:payproc_main',
-            'manta-wallet=manta.testing.__main__:wallet_main',
+        "console_scripts": [
+            "manta-runner=manta.testing.__main__:main",
+            "manta-store=manta.testing.__main__:store_main",
+            "manta-payproc=manta.testing.__main__:payproc_main",
+            "manta-wallet=manta.testing.__main__:wallet_main",
         ],
     },
-
 )
